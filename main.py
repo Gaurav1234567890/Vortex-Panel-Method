487b337b5524e6b3c0f06704e0a29ef59351383f
Gaurav1234567890 <38824354+Gaurav1234567890@users.noreply.github.com>
2024-05-10 16:27:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import tkinter as tk
+from PIL import ImageTk,Image
+import calculation as cal
+
+root = tk.Tk()
+root.title(".dat file selector")
+
+print("Vortex Panel Solver by: Gaurav B\n")
+print("ver 1.0")
+print("---------------\n")
